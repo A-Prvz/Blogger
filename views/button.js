8ac7d8b71de39eec56096f5330c7979226cc5696
A_Prvz <parvejarman817@gmail.com>
2022-11-14 03:13:29 +0530

    edit

--- views/button.js
+++ views/button.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function onClick() {
-  btn.style.backgroundColor = 'salmon';
+  btn.style.backgroundColor = '#0085A1';
   btn.style.color = 'white';
 });
